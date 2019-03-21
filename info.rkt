fe91d9251542df5f9edb41fb457fb6c7f548d425
Spencer Florence <spencer@florence.io>
2019-03-21 07:50:36 -0500

    Add missing dep

--- info.rkt
+++ info.rkt
@@ -1,7 +1,10 @@
 #lang info
 (define collection "datacell")
 (define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("sandbox-lib"
+                     "scribble-lib"
+		     "racket-doc"
+		     "rackunit-lib"))
 (define scribblings '(("scribblings/datacell.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
